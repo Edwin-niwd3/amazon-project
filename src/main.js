8dbfb60fb5b77fcf96b8e98d654e044523c3a63f
Edwin-niwd3 <edwin.navarro@laverne.edu>
2024-04-24 19:19:08 -0700

    Add in button, and more CSS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {products} from './data/products';
+import formatCurrency from './utils/money';
 
 
 const Main = () => {
@@ -17,6 +18,28 @@ const Main = () => {
                 </div>
                 <div className="product-rating-container">
                   <img src= {require(`./images/ratings/rating-${product.rating.stars * 10}.png`)} className="product-rating-stars"/>
+                  <div className="product-rating-count link-primary">
+                    {`${product.rating.count}`}
+                  </div>
+                </div>
+
+                <div className="product-price">
+                  {`${formatCurrency(product.priceCents)}`}
+                </div>
+
+                <div className="product-quantity-container">
+                  <select id = {`product-Quantity-${product.id}`}>
+                  <option selected value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                  </select>
                 </div>
               </div>
             ))
